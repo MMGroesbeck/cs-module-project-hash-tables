d54349e764eaac50a6b5298fd47e76366bd004c0
M Groesbeck <m.groesbeck@gmail.com>
2020-08-05 07:55:21 -0700

    Decoded text for crack_caesar.py is readable; I do not see any letters out of place, only some punctuation-handling oddities.

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,36 @@
 # decode it.
 
 # Your code here
+char_chart = {}
+letters = "ETAOHNRISDLWUGFBMYCPKVQJXZ"
+with open("ciphertext.txt") as txt:
+    ciphered = txt.read()
 
+# Count frequency of each letter in ciphertext:
+for char in ciphered:
+    if char in char_chart:
+        char_chart[char] += 1
+    elif char in letters:
+        char_chart[char] = 1
+
+# Sort characters in ciphertext by frequency descending:
+chars_found = char_chart.keys()
+freq_tuples = []
+for char in chars_found:
+    freq_tuples.append((char, char_chart[char]))
+freq_tuples = sorted(freq_tuples, key=lambda char: char[1], reverse=True)
+
+# Combine sorted list with letter frequency order into decoding dictionary:
+decoder = {}
+for i in range(26):
+    decoder[freq_tuples[i][0]] = letters[i]
+
+# Decode ciphertext:
+decoded = ""
+for char in ciphered:
+    if char in decoder:
+        decoded = decoded + decoder[char]
+    else:
+        decoded = decoded + char
+
+print(decoded)
