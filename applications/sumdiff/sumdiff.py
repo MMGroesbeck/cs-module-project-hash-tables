2600f36d1a246cb37e7d542fd5aea0b697d9ea3c
M Groesbeck <m.groesbeck@gmail.com>
2020-08-06 12:03:26 -0700

    Worked on other version based on shared plan; mine does not seem to ahave a major disadvantage.

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -4,9 +4,9 @@ f(a) + f(b) = f(c) - f(d)
 """
 import itertools
 
-# q = set(range(1, 10))
-q = set(range(1, 200))
-# q = (1, 3, 4, 7, 12)
+# q1 = set(range(1, 10))
+q2 = set(range(1, 100))
+# q3 = (1, 3, 4, 7, 12)
 
 
 def f(x):
@@ -33,21 +33,57 @@ def f(x):
 
 # Version 2:
 # charts is [diff_chart, sum_chart]
-charts = [{},{}]
-out = ""
-for i,j in itertools.product(q,q):
-    vals = (f(i)-f(j), f(i)+f(j))
-    for ind in range(2):
-        if vals[ind] in charts[ind]:
-            charts[ind][vals[ind]].append((i,j))
-        else:
-            charts[ind][vals[ind]] = [(i,j)]
-for diff in charts[0]:
-    if diff in charts[1]:
-        for i,j in itertools.product(charts[0][diff], charts[1][diff]):
-            vars = f"f({j[0]}) + f({j[1]}) = f({i[0]}) - f({i[1]})"
-            vals = f"{f(j[0])} + {f(j[1])} = {f(i[0])} - {f(i[1])}"
-            # print(f"{vars:30} {vals}")
-            out = out + f"{vars:30} {vals}\n"
+def sumdiff(q):
+    charts = [{},{}]
+    out = ""
+    for i,j in itertools.product(q,q):
+        vals = (f(i)-f(j), f(i)+f(j))
+        for ind in range(2):
+            if vals[ind] in charts[ind]:
+                charts[ind][vals[ind]].append((i,j))
+            else:
+                charts[ind][vals[ind]] = [(i,j)]
+    for diff in charts[0].keys():
+        if diff in charts[1]:
+            for i,j in itertools.product(charts[0][diff], charts[1][diff]):
+                vars = f"f({j[0]}) + f({j[1]}) = f({i[0]}) - f({i[1]})"
+                vals = f"{f(j[0])} + {f(j[1])} = {f(i[0])} - {f(i[1])}"
+                # print(f"{vars:30} {vals}")
+                out = out + f"{vars:30} {vals}\n"
+    return out
 
-# print(out)
+# # Version 3: looks messier?
+# # Still slower than shared code which is almost identical! ...due to different q
+# def sumdiff(q):
+#     sums = {}
+#     diffs = {}
+#     out = ""
+#     for i in q:
+#         for j in q:
+#             this_diff = f(i) - f(j)
+#             #
+#             if this_diff not in diffs:
+#                 diffs[this_diff] = [(i,j)]
+#             else:
+#                 diffs[this_diff].append((i,j))
+            
+#             #
+#             this_sum = f(i) + f(j)
+#             if this_sum not in sums:
+#                 sums[this_sum] = [(i,j)]
+#             else:
+#                 sums[this_sum].append((i,j))
+
+#     for curr in sums:
+#         t_s = sums[curr]
+#         if curr in diffs:
+#             t_d = diffs[curr]
+#         else:
+#             t_d = []
+#         for sum_pair in t_s:
+#             for diff_pair in t_d:
+#                 out = out + f"f({sum_pair[0]}) + f({sum_pair[1]}) = f({diff_pair[0]}) - f({diff_pair[1]})    {f(sum_pair[0])} + {f(sum_pair[1])} = {f(diff_pair[0])} - {f(diff_pair[1])}"
+#     return out
+
+returned = sumdiff(q2)
+# print(returned)
