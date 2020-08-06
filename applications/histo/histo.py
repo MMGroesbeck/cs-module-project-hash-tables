d884f588db1708c2cc000942acf9d9b9235593ac
M Groesbeck <m.groesbeck@gmail.com>
2020-08-06 09:03:30 -0700

    Cleaned up comment.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -35,7 +35,6 @@ def histo(filename):
         word_tups = sorted(word_tups, key=lambda item: item[i[0]], reverse=i[1])
 
     for word_tup in word_tups:
-        # print(word_tup[0], "#"*word_tup[1])
         print(f"{word_tup[0]:{max_len+2}}{mark*word_tup[1]}")
 
 histo("robin.txt")
